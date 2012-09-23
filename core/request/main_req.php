f5d87d10f7fc19e5bc3d9e6bc588d55b1e6f6c55
Joshua Kidd <kiddj2015@gmail.com>
2012-09-23 14:34:01 -0500

    TODO: Add Custom css and remove Bootstrap.

--- core/request/main_req.php
+++ core/request/main_req.php
@@ -1,10 +1,12 @@
 <?php
-require_once('global.php');
+require_once('../global.php');
+
 // This file contains all the main site JSON, AJAX, and other Request Types.
-$db = new DB;
+
 class Request {
 	
 	public static function login_req($vars) {
+		global $db;
 		$email = $vars['email'];
 		$pass = $vars['pass'];
 		
@@ -12,10 +14,13 @@ class Request {
 			echo "empty_fields";
 		}else{
 			
+			$query = $db->query("SELECT guid, first, username, password, salt, email FROM bc_users_entity WHERE 'username'='{$email}' OR 'email'='{$email}'");
+			echo $count = $query->rowCount();
 		}
 	}
 	
 	public static function register_req($vars) {
+		global $db;
 		$first = $vars['first'];
 		$last = $vars['last'];
 		$email = $vars['email'];
@@ -29,14 +34,41 @@ class Request {
 			$last = strtolower(ucfirst($last));
 			$name = "{$first} {$last}";
 			
-			//$query = $db->query("SELECT email FROM users WHERE email='{$email}'");
+			$pass = hash('sha512', $pass);
+			$dob = 
 			
-			//$reg = $db->prepare('INSERT INTO users (first, last, name, email, pass) VALUES ()');
-			//$reg->execute(array());
+			$query = $db->query("SELECT email FROM bc_users_entity WHERE email='{$email}'");
+			$count = $query->rowCount();
+			
+			if($count != "0") {
+				echo "already_exists";
+			}else{
+				$array = array(
+					'guid' => "",
+					'first' => $first,
+					'last' => $last,
+					'name' => $name,
+					
+				);
+				$reg = $db->query("INSERT INTO bc_users_entity (guid, first, last, name, username, password, salt, email, code) VALUES (?, ?, ?, ?)", $array);
+			}
+			
+			
+			// $reg = $db->prepare('INSERT INTO users (first, last, name, email, pass) VALUES ()');
+			// $reg->execute(array());
 		}
 		
 	}
 	
 }
+
+$vars = array(
+	'first' => "Joshua",
+	'last' => "kidd",
+	'dob' => "848275200",
+	'email' => "kiddj2015@gmail.com",
+	'pass' => "JoshuaK123"
+);
+Request::login_req($vars);
 
 ?>
